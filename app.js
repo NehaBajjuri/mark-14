332d2db753f60f4573080bf4f7772d6ca3b9b66d
NehaBajjuri <105218064+NehaBajjuri@users.noreply.github.com>
2022-10-27 21:20:49 +0530

    commit

---
+++ app.js
@@ -0,0 +1,3 @@
+var initialPrice = document.querySelector('#initial-price');
+var stocksQuantity = document.querySelector('#stocks-quantity');
+var currentprice = document.quereySelector('#current-price');
